c218866f787057fb9cc2b7a7016926d322910a6e
Greg Thornton <xdissent@me.com>
2014-09-03 23:03:29 -0500

    Update fish env template

--- templates/ruby.fish
+++ templates/ruby.fish
@@ -1,10 +1,15 @@
-# Configure and activate rbenv. You know, for rubies.
-
-set -x RBENV_ROOT $BOXEN_HOME/rbenv
-
-set -x PATH $BOXEN_HOME/rbenv/bin $BOXEN_HOME/rbenv/plugins/ruby-build/bin $PATH
+# Put ruby-build on PATH
+set -x PATH <%= scope.lookupvar("::ruby::build::prefix") %>/bin $PATH
 
 # Allow bundler to use all the cores for parallel installation
 set -x BUNDLE_JOBS <%= scope.lookupvar("::processorcount") %>
 
+<%- if scope.lookupvar("::ruby::provider") == "rbenv" -%>
+# Configure RBENV_ROOT and put RBENV_ROOT/bin on PATH
+set -x RBENV_ROOT <%= scope.lookupvar("::ruby::rbenv::prefix") %>
+set -x PATH $RBENV_ROOT/bin $PATH
+
+# Load rbenv
 . (rbenv init - fish | psub)
+
+<%- end -%>
